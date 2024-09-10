5ffff6660ffa4835f73a5ac490cc02852d9be5f7
Cueshin <tsoyvladislav45@gmail.com>
2024-09-10 22:21:38 +0900

    2D+UI made + 1920x1080 fixed

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    static bool once = false;
+    static bool once1 = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // foreach (Transform children in GameObject.FindWithTag("Block").transform)
+        //{
+        /* if (GameObject.FindWithTag("Block").transform.position.x == 5 && transform.position.y <= 2 && transform.position.z == 5 && once == false )
+         {
+             once = true;
+             Debug.Log("score +1");
+             score = score + 1;
+
+         }
+
+         // }
+     }
+     void OnTriggerEnter(Collider objectName)
+     {
+         Debug.Log("Entered collision with" + objectName.gameObject.name);
+     }*/
+    }
+}
